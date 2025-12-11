0bbfca66e004ef2d4ddd1f3a4b20aec8dc3e2551
Nadiiaas <144695748+Nadiiaas@users.noreply.github.com>
2025-12-11 17:13:05 +0100

    Update test.cy.js

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,57 +1,44 @@
 const usersData = require('../fixtures/users.json');
 
-describe('Technical Task', () => {
-  
-  let realUserAgent = '';
+describe('API Task', () => {
 
-  beforeEach(() => {
+  it('GET - List Users', () => {
 
-    cy.visit('/');
-    
-    cy.get('.logo', { timeout: 30000 }).should('be.visible');
+    cy.request('GET', '/api/users?page=2').then((response) => {
 
-    cy.window().then((win) => {
-      realUserAgent = win.navigator.userAgent;
-    });
-  });
-
-  it('GET - List Users (Page 2)', () => {
-    cy.request({
-      method: 'GET',
-      url: '/api/users?page=2',
-      headers: {
-        'User-Agent': realUserAgent
-      }
-    }).then((response) => {
       expect(response.status).to.eq(200);
+
       expect(response.body).to.have.property('total');
+      expect(response.body).to.have.property('data');
+      expect(response.body.data).to.be.an('array');
+
       expect(response.body.data[0].last_name).to.eq('Lawson');
-      
-      const receivedCount = response.body.data.length;
+      expect(response.body.data[1].last_name).to.eq('Ferguson');
+
+      const dataCount = response.body.data.length;
       const totalCount = response.body.total;
-      expect(receivedCount).to.be.lte(totalCount);
+      expect(dataCount).to.be.lte(totalCount);
 
       expect(response.body.page).to.be.a('number');
-      expect(response.body.data).to.be.an('array');
     });
   });
 
   usersData.forEach((user) => {
     it(`POST - Create User: ${user.name}`, () => {
-      cy.request({
-        method: 'POST',
-        url: '/api/users',
-        body: user,
-        headers: {
-          'User-Agent': realUserAgent
-        }
-      }).then((response) => {
+
+      const limit = 1000; 
+
+      cy.request('POST', '/api/users', user).then((response) => {
+
         expect(response.status).to.eq(201);
+
         expect(response.body).to.have.property('id');
+        expect(response.body).to.have.property('createdAt');
+
         expect(response.body.name).to.eq(user.name);
         expect(response.body.job).to.eq(user.job);
-        
-        expect(response.duration).to.be.lessThan(4000);
+
+        expect(response.duration).to.be.lessThan(limit);
       });
     });
   });
